5c46425a46e7d25e3615de946f7d3af6854e6b56
Greg Scott <i@gregoryjscott.com>
2011-01-30 17:40:03 -0900

    hooked up sign in and new user to controllers

---
+++ MvcTemplate/Areas/Security/Controllers/UserSessionsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using MvcTemplate.Models;
+
+namespace MvcTemplate.Areas.Security.Controllers
+{
+    public class UserSessionsController : Controller
+    {
+        [HttpPost]
+        public ActionResult Create(LogOnModel logOnModel)
+        {
+            var membershipService = new AccountMembershipService();
+            if (ModelState.IsValid && membershipService.ValidateUser(logOnModel.UserName, logOnModel.Password))
+            {
+                var formsService = new FormsAuthenticationService();
+                formsService.SignIn(logOnModel.UserName, false);
+
+                return Json(new { Success = true });
+            }
+
+            return Json(new { Success = false });
+        }
+
+        [HttpPost]
+        public ActionResult Delete()
+        {
+            var formsService = new FormsAuthenticationService();
+            formsService.SignOut();
+
+            return Json(new { Success = true });
+        }
+    }
+}
---
+++ MvcTemplate/Areas/Security/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcTemplate.Models;
+
+namespace MvcTemplate.Areas.Security.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Create(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var membershipService = new AccountMembershipService();
+                MembershipCreateStatus createStatus = membershipService.CreateUser(model.UserName, model.Password, model.Email);
+
+                if (createStatus == MembershipCreateStatus.Success)
+                {
+                    return Json(new { Success = true });
+                }
+
+            }
+
+            return Json(new { Success = false });
+        }
+    }
+}
